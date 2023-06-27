18906f0e0fdc0dd87db6f816c4dc22fd96a28714
zizo <t.zouhir.zouhir@gmail.com>
2023-06-27 08:20:12 -0700

    Add project files.

---
+++ ExempleAngularWithAPIDotNetCore/Controllers/ToDoAppController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ExempleAngularWithAPIDotNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public ToDoAppController(IConfiguration configuration)
+        {
+
+            _configuration = configuration;
+
+        }
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes()
+        {
+            string query = "select * from notes";
+            DataTable dt=new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand=new SqlCommand(query,myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    dt.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();  
+                }
+            }
+            return new JsonResult(dt);
+
+        }
+
+        [HttpPost]
+        [Route("AddNotes")]
+        public JsonResult AddNotes([FromForm] string newNotes )
+        {
+            string query = "insert into  notes values(@newNotes)";
+            DataTable dt = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@newNotes",newNotes);
+                    myReader= myCommand.ExecuteReader();
+                    dt.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("added succefully");
+
+        }
+
+        [HttpDelete]
+        [Route("DeleteNotes")]
+        public JsonResult DeleteNotes(int id)
+        {
+            string query = "delete from notes where id=@id";
+            DataTable dt = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myReader = myCommand.ExecuteReader();
+                    dt.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("delete succefully");
+
+        }
+    }
+
+
+}
